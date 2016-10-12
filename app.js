1cab6e797dcbbdd63243ea46f021f06d3981b7a4
Salvador <salvadorrod13@gmail.com>
2016-10-11 20:27:29 -0500

    My First web done!

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
+const express = require('express');
 
+const app = express();
+
+app.get('/:message', function(request, response, next){
+  // const message = request.params.message;
+  // console.log(message);
+  const {message} = request.params;
+
+  return response.json(message);
+
+});
+
+// Set our port to server the application on
+const port = 3000;
+
+// Tell our instance of express to listen to request made on our port
+app.listen(port, function() {
+  console.log(`Listening on port: ${port}`);
+});
